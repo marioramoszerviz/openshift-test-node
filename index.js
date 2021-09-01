41f8fd81d51a5c2f2723312285972805cbb5e14c
Alejandro Plaza <alejandro.plaza@zervizgroup.com>
2021-09-01 15:13:41 -0500

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function (request, response){
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World from expressjs in a Docker container.");
+  response.end("Hello World from expressjs in a Docker container 2.");
 });
 
 server.listen(8080);
